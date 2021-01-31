d236801f7ed2d4afda8ab9db6d90da4588577d05
lucamadd <44671856+lucamadd@users.noreply.github.com>
2021-01-31 22:03:02 +0100

    added email logic

--- helper.py
+++ helper.py
@@ -1,30 +1,42 @@
 from flask.templating import render_template
 from flask_mail import Mail, Message
-import smtplib
-
-def deprecated_send_registration_email(mail, recipient, name):
-    try:
-        msg = Message( 
-                    subject = 'Registrazione effettuata', 
-                    sender = 'info.vaccitaly@gmail.com', 
-                    recipients = [recipient] 
-                ) 
-        msg.body = f'Ciao, {name}.\n\nBenvenuto su VaccItaly.'
-        mail.send(msg) 
-    except Exception as e:
-        print(f"exception is {e}")
-        return False
-    return True
+import smtplib, markdown
 
 def send_registration_email(recipient, name):
-    s = smtplib.SMTP('smtp.gmail.com', 587) 
+    server = smtplib.SMTP('smtp.gmail.com', 587) 
   
-    s.starttls() 
+    server.starttls() 
     
-    s.login("info.vaccitaly@gmail.com", "VaccItaly2021") 
-    
-    message = f"Ciao {name}, benvenuto su VaccItaly."
-    
-    s.sendmail("VaccItaly Support", recipient, message) 
-    
-    s.quit() 
+    server.login("info.vaccitaly@gmail.com", "VaccItaly2021")
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+
+    multipart_msg = MIMEMultipart("alternative")
+
+    multipart_msg["Subject"] = 'Registrazione effettuata'
+    multipart_msg["From"] = 'VaccItaly Support' + f' <info.vaccitaly@gmail.com>'
+    multipart_msg["To"] = recipient
+
+    message = f'Ciao {name}, grazie per esserti registrato a VaccItaly.'
+
+    text = message
+    html = f'<html>\
+            <body>\
+            <p>Ciao {name},<br>\
+            grazie per esserti registrato a VaccItaly. Se già sai come funziona il sito, puoi prenotare un appuntamento\
+            <a href="#">cliccando qui</a>.</p>\
+            <p>Se vuoi sapere di più su come funziona, ti consigliamo di dare un\'occhiata <a href="#">qui</a>.</p>\
+            <p></p>\
+            <p></p>\
+            <p style="margin-top=3em;" >Il team di VaccItaly</p>\
+            </body>\
+            </html>'
+
+    part1 = MIMEText(text, "plain", "utf-8")
+    part2 = MIMEText(html, "html", "utf-8")
+
+    multipart_msg.attach(part1)
+    multipart_msg.attach(part2)
+
+    server.sendmail('info.vaccitaly@gmail.com', recipient,
+                                multipart_msg.as_string().encode('utf-8'))
